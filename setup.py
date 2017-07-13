d9e754dfac78667bb55cafbb6f0f8ff4fa6bb8dc
Nic McDonald <n.mcdonald83@gmail.com>
2017-07-13 16:52:26 -0600

    migrated to using NOTICE file

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 """
- * Copyright (c) 2012-2016, Nic McDonald
- * All rights reserved.
- *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
@@ -12,9 +9,12 @@
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
  *
- * - Neither the name of prim nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
+ * - Neither the name of prim nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior
+ * written permission.
+ *
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -28,6 +28,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
 """
+
 import codecs
 import re
 import os
